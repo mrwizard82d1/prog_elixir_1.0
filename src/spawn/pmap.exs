14f1f1f4f91a2c7a94e62911cb3d34d4dfa1b196
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2014-10-18 20:06:15 +0100

    Updated pmap module to P1.0 of book.

--- src/spawn/pmap.exs
+++ src/spawn/pmap.exs
@@ -1,19 +1,12 @@
 defmodule Parallel do
 	def pmap(collection, each_f) do
-		me = self
 		collection
-		|> Enum.map(fn (item) ->
-										 spawn_link fn -> (send me,
-																			 {self, each_f.(item)})
-																end
-								end)
-		|> Enum.map(fn (pid) ->
-										 receive do
-											 { ^pid, result } -> result
-										 end
-								end)
+		|> Enum.map(&(Task.async(fn -> each_f.(&1) end)))
+		|> Enum.map(&(Task.await/1))
 	end
 end
 
 :io.format "~w~n", [(Enum.take (Parallel.pmap 1..1000, &(&1 * &1)), 10)]
 :io.format "~w~n", [(Enum.drop (Parallel.pmap 1..1000, &(&1 * &1)), 990)]
+# Trying (but failing) to use all my cores. Sigh.
+# :io.format "~w~n", [(Parallel.pmap 1..1000, &(&1 * &1))]
