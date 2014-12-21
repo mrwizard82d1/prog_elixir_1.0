487ccc848ab6fe03bf2eaa09fcc82e7f9615c886
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2014-12-21 00:47:03 +0000

    Updated Guard section for version 1.0 of book.

--- src/mm/factorial2.exs
+++ src/mm/factorial2.exs
@@ -1,11 +1,13 @@
-defmodule Factorial do
+defmodule Factorial2 do
 	def of(0), do: 1
 	def of(n) when n > 0, do: n * of(n-1)
 end
 
-IO.puts "Factorial.of(3)=#{Factorial.of(3)}"
-IO.puts "Factorial.of(7)=#{Factorial.of(10)}"
-IO.puts "Factorial.of(10)=#{Factorial.of(10)}"
-IO.write "Factorial.of(1000)="
-large_factorial_start = String.slice "#{Factorial.of(1000)}", 0..32
+IO.puts "Factorial2.of(3)=#{Factorial2.of(3)}"
+IO.puts "Factorial2.of(7)=#{Factorial2.of(10)}"
+IO.puts "Factorial2.of(10)=#{Factorial2.of(10)}"
+IO.write "Factorial2.of(1000)="
+large_factorial_start = String.slice "#{Factorial2.of(1000)}", 0..32
 IO.puts "#{large_factorial_start}..."
+IO.puts("Factorial of a negative number fails: Factorial2.of(-17)")
+Factorial2.of(-17)
