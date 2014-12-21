a5ba35c57b8c62d63bff6eb76415bcdc34a24f7e
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2014-12-21 03:15:44 +0000

    Revised chop to be clearer for version 1.0 of book.

--- src/your_turn/chop.exs
+++ src/your_turn/chop.exs
@@ -1,17 +1,20 @@
 defmodule Chop do
 	def guess(n, n..n), do: n
-	def guess(n, lower..upper) when div(lower + upper, 2) == n, do: n
 	def guess(n, lower..upper) do
 		the_guess = div(lower + upper, 2)
-		IO.puts "Is it #{the_guess}?"
-		if is_lower(the_guess, n) do
-			guess(n, the_guess..upper)
-		else
-			guess(n, lower..the_guess)
-		end
+		IO.puts("Is it #{the_guess}?")
+    guess_helper(the_guess, n, lower..upper)
 	end
 
-	def is_lower(the_guess, n), do: the_guess < n
+  defp guess_helper(the_guess, n, lower.._) when the_guess > n do
+    guess(n, lower..the_guess-1)
+  end
+  defp guess_helper(the_guess, n, _..upper) when the_guess < n do
+    guess(n, the_guess+1..upper)
+  end
+  defp guess_helper(the_guess, n, _) do
+    guess(n, the_guess..the_guess)
+  end
 end
 
 IO.puts "Chop.guess(3, 3..3)=#{Chop.guess(3, 3..3)}"
