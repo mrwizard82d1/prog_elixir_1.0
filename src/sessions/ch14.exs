5cea187e0de03f84908eb5e0ef19ec365f859f2c
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2014-12-28 07:03:37 +0000

    Illustrates hang: sending message to dead process.

--- src/sessions/ch14.exs
+++ src/sessions/ch14.exs
@@ -17,3 +17,31 @@ receive do
 	{:ok, message} ->
 		IO.puts(message)
 end
+
+# A hung process while waiting for responses from a dead process.
+IO.puts("\nIllustrate a process hanging while waiting for a message")
+IO.puts("from a dead process.")
+
+IO.puts("Spawn the (single-shot) greet process.")
+pid = spawn(Spawn2, :greet, [])
+
+IO.puts("Send a message and...")
+send(pid, {self, "Concurrent World"})
+
+IO.puts("...wait for the response.")
+receive do
+	{:ok, response} ->
+		IO.puts(response)
+end
+
+# Uncomment the following region to illustrate hanging.
+# IO.puts("\n!!! WARNING !!! This code will hang.")
+# IO.gets("Press any key when ready...")
+# IO.puts("Now send the second message and...")
+# send(pid, {self, "Another Elixir World"})
+
+# IO.puts("...hang waiting for a response from a 'dead' greet process.")
+# receive do
+# 	{:ok, response} ->
+# 		IO.puts(response)
+# end
