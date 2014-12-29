28b1f449a0d3c8ee72741d59efd280930cae41b6
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2014-12-29 13:38:40 +0000

    Send messages to two processes.

---
+++ src/your_turn/fred_wilma.exs
@@ -0,0 +1,44 @@
+require Dump
+
+
+defmodule FredWilma do
+	
+	def start() do
+		spawn(FredWilma, :subscribe, [])
+	end
+
+	def subscribe() do
+		receive do
+			{requester, token} ->
+				send(requester, {:ok, token})
+				subscribe()
+			:stop ->
+				true
+		end
+	end
+
+	def publish(pid, token) do
+		send(pid, token)
+	end
+
+	def stop(pid) do
+		send(pid, :stop)
+	end
+	
+end
+			
+process1 = FredWilma.start()
+process2 = FredWilma.start()
+FredWilma.publish(process1, {self, :fred})
+FredWilma.publish(process2, {self, :wilma})
+
+receive do
+	result ->
+		IO.inspect(result)
+end
+receive do
+	result ->
+		IO.inspect(result)
+end
+send(process1, :stop)
+send(process2, :stop)
