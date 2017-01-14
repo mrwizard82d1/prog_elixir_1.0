ad42dc4ad8a3aba571e4926bb1f618d9d1aa43a7
Larry Jones <larryj@blinds.com>
2017-01-14 17:59:35 -0600

    Illustrating "complex" pattern matching

--- src/iex/complex_matching.exs
+++ src/iex/complex_matching.exs
@@ -43,3 +43,41 @@ Helper.read "b"
 Helper.print b
 Helper.read "c"
 Helper.print c
+
+Helper.blank_line
+Helper.comment "Matching a nested list"
+Helper.read "list = [1, 2, [3, 4, 5]]"
+list = [1, 2, [3, 4, 5]]
+Helper.print list
+Helper.read "[a, b, c] = list"
+[a, b, c] = list
+Helper.print list
+Helper.comment "a and b are scalar value"
+Helper.read "a"
+Helper.print a
+Helper.read "b"
+Helper.print b
+Helper.comment "But c matches an entire list"
+Helper.read "c"
+Helper.print c
+
+Helper.blank_line
+Helper.comment "Matching a list with literal values"
+Helper.read "list = [1, 2, 3]"
+list = [1, 2, 3]
+Helper.print list
+Helper.read "[a, 2, b] = list"
+[a, 2, b] = list
+Helper.comment "The match succeeds because both lists have a 2 as the second item"
+Helper.read "a"
+Helper.print a
+Helper.read "b"
+Helper.print b
+
+Helper.blank_line
+Helper.comment "But if literals **do not** match, the match **fails**"
+Helper.read "list = [1, 2, 3]"
+list = [1, 2, 3]
+Helper.print list
+Helper.read "[a, 1, b] = list"
+[a, 1, b] = list
