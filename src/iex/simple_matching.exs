059eec5bda8f8cc136241786d8a755821470df6d
Larry Jones <larryj@blinds.com>
2017-01-14 13:42:45 -0600

    Code illustrating simple matching

---
+++ src/iex/simple_matching.exs
@@ -0,0 +1,23 @@
+IO.puts "Assignment"
+IO.puts "a = 1"
+a = 1
+IO.puts a
+
+IO.write "\n"
+IO.puts "Evaluation"
+IO.puts "a + 3"
+IO.puts a + 3
+
+IO.write "\n"
+IO.puts "Matching!"
+IO.puts "a = 1"
+a = 1
+IO.puts a
+
+IO.puts "1 = a"
+IO.puts(1 = a)
+
+IO.write "\n"
+IO.puts "And **no** match"
+IO.puts "2 = a"
+2 = a
