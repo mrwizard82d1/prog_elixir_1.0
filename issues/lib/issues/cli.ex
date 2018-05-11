77ac0b9441a5a31fae3ca375e97cb739f60ab692
Larry Jones <larry.jones@reveal-energy.com>
2018-05-11 12:30:36 -0500

    Stub call to Issues.GithubIssues.fetch

--- issues/lib/issues/cli.ex
+++ issues/lib/issues/cli.ex
@@ -51,9 +51,7 @@ defmodule Issues.CLI do
     System.halt(0)
   end
 
-  def process({user, project, count}) do
-    IO.puts """
-  Fetching #{count} items for project, #{project}, of user, #{user}.
-"""
+  def process({user, project, _count}) do
+    Issues.GithubIssues.fetch(user, project)
   end
 end
