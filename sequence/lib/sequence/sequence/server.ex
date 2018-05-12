3087b460d8d0d299fa95cbf85b18bc9cfec48c40
Larry Jones <larry.jones@reveal-energy.com>
2018-05-12 08:04:47 -0500

    Get the next number from our server

--- sequence/lib/sequence/sequence/server.ex
+++ sequence/lib/sequence/sequence/server.ex
@@ -1,23 +1,23 @@
 defmodule Sequence.Server do
-  @moduledoc false
-  
-
+  @moduledoc """
+  Our first GenServer.
+"""
 
   use GenServer
 
-  def start_link(state, opts) do
-    GenServer.start_link(__MODULE__, state, opts)
+#  def start_link(state, opts) do
+#    GenServer.start_link(__MODULE__, state, opts)
+#  end
+
+  def init(starting_at) do
+    {:ok, starting_at}
   end
 
-  def init(_opts) do
-    {:ok, %{}}
+  def handle_call(:next_number, _from, current_number) do
+    {:reply, current_number, current_number + 1}
   end
 
-  def handle_call(_msg, _from, state) do
-    {:reply, :ok, state}
-  end
-
-  def handle_cast(_msg, state) do
-    {:noreply, state}
-  end
+#  def handle_cast(_msg, state) do
+#    {:noreply, state}
+#  end
 end
